1d78093e8a7c7e327032d85ddf0ac63186a5977f
Xavier Julian Rodriguez <106761585+Xavier-Julian-Rodriguez@users.noreply.github.com>
2022-10-26 23:08:50 -0400

    function that determines bmi

---
+++ codeWarsChallenges/jsChallenges/calculateBmi.js
@@ -0,0 +1,24 @@
+/*Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+if bmi <= 25.0 return "Normal"
+if bmi <= 30.0 return "Overweight"
+if bmi > 30 return "Obese"*/
+
+
+//My solution
+function bmi(weight, height) {
+    const bodyMassIndex = weight/(height ** 2);
+    if(bodyMassIndex <= 18.5) {
+      return 'Underweight';
+    } else if(bodyMassIndex <= 25.0) {
+      return 'Normal';
+    } else if(bodyMassIndex <= 30.0) {
+      return 'Overweight';
+    } else {
+      return 'Obese';
+    }
+  }
+
+  //Uncomment line 24 to test it out. Should Return 'Normal'.
+  //console.log(bmi(80, 1.80))

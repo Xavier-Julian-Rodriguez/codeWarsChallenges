e2dd940e4fb8ec8af4045334a44cc871c9610f22
Xavier Julian Rodriguez <106761585+Xavier-Julian-Rodriguez@users.noreply.github.com>
2022-12-06 11:02:50 -0500

    javaScript fundamentals practice challenges

---
+++ codeWarsChallenges/jsChallenges/centuryFrom.js
@@ -0,0 +1,22 @@
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+
+// Given a year, return the century it is in.
+
+// Examples
+
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+
+
+//my solution strategy takes in a given year, divides that integer by 100, and then rounds it up using Math.ceil()
+function century(year) {
+    // Finish this :)
+    return Math.ceil(year/100)
+    
+    
+  }
---
+++ codeWarsChallenges/jsChallenges/messiGoals.js
@@ -0,0 +1,22 @@
+// Messi goals function
+
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+
+
+
+//my solution strategy takes in variables that each contain an integer value and then adds them together to return the total amount of goals.
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // code goes here
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }

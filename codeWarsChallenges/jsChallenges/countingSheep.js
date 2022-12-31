55fb211615f8f2033d85602702f3ed6b4f36f686
Xavier Julian Rodriguez <106761585+Xavier-Julian-Rodriguez@users.noreply.github.com>
2022-12-30 19:46:51 -0500

    fundamental js challenges from 12/30/22

---
+++ codeWarsChallenges/jsChallenges/countingSheep.js
@@ -0,0 +1,30 @@
+// DESCRIPTION:
+
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+
+//----------------My Solution---------------
+//Counting Sheep
+function countSheeps(arrayOfSheep) {
+    //true = present, false = absent, include null/undefined, iterate through the array and add to a count variable
+    let totalSheep = 0
+    for(let i = 0; i < arrayOfSheep.length ; i++) {
+      if(arrayOfSheep[i] === true) {
+        totalSheep += 1;
+      } else {
+        console.log('null/undefined');
+      }
+    }  return totalSheep;
+  }
---
+++ codeWarsChallenges/jsChallenges/printingErrors.js
@@ -0,0 +1,41 @@
+// DESCRIPTION:
+
+// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from ato z.
+
+// Examples:
+
+// s="aaabbbbhaijjjm"
+// printer_error(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// printer_error(s) => "8/22"
+
+
+
+//----------------My Solution---------------
+//Printing Errors
+function printerError(s) {
+  
+    //interpolate a string where 'numerator' is the total number of errors
+    //use s.length for string total count
+    //use .includes() on variable holding letters from range n-z.
+    //iterate through the array given
+    let numerator = 0;
+   
+    let lettersNToZ = "nopqrstuvwxyz"
+    
+    for(let i = 0; i < s.length; i++) {
+      if(lettersNToZ.includes(s[i])) {
+        numerator += 1;
+      }
+    }
+    return `${numerator}/${s.length}`
+  }
